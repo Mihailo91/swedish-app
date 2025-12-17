84a44e438572922f074f7003c3f08c06d43f272d
Mihailo Krstic <mihailo.krstic@eng.it>
2025-12-18 00:14:12 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+//Error middleware
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,13 +15,20 @@ app.use(express.json());
 //to accept form data
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/health', (req, res) => {
-  res.status(200).json({message : 'UP!'});
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "UP!" });
 });
 
 // Newsletter subscription route
-app.use('/api/subscribe', require('./routes/newsletterRoutes'));
+app.use("/api/subscribe", require("./routes/newsletterRoutes"));
 
-app.use (errorHandler);
+app.use(errorHandler);
+app.use(
+  cors({
+    origin: "https://swedishmyway.com",
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
