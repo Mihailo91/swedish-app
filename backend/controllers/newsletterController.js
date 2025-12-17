2062b871dc36ba0cf9b532231707c8a77051300d
Mihailo Krstic <mihailo.krstic@eng.it>
2025-12-17 23:59:41 +0100

    Added implementation for subscibe api

--- backend/controllers/newsletterController.js
+++ backend/controllers/newsletterController.js
@@ -1,4 +1,10 @@
 const asyncHandler = require("express-async-handler");
+const dotenv = require("dotenv").config();
+const axios = require("axios");
+
+const MAILER_LITE_URL = process.env.NODE_MAILER_LITE_URL;
+const MAILER_LITE_GROUP_ID = process.env.NODE_MAILER_LITE_GROUP_ID;
+const MAILER_LITE_TOKEN = process.env.NODE_MAILER_LITE_TOKEN;
 
 const subscribeUser = asyncHandler(async (req, res) => {
   const { name, email } = req.body;
@@ -7,6 +13,34 @@ const subscribeUser = asyncHandler(async (req, res) => {
     throw new Error("Name and email are required");
   }
 
-  res.status(200).json({ message: `Subscription successful for ` });
+  const requestData = {
+    email: email,
+    fields: {
+      name: name,
+    },
+    groups: [MAILER_LITE_GROUP_ID],
+  };
+  try {
+    const response = await axios.post(MAILER_LITE_URL, requestData, {
+      headers: {
+        Authorization: `Bearer ${MAILER_LITE_TOKEN}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    console.log("calling mailer lite");
+    res.status(200).json({
+      success: true,
+      message: "User subscribed successfully",
+      data: response.data,
+    });
+    console.log("finished calling mailer lite");
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500);
+    throw new Error("Subscription failed");
+  }
 });
+
 module.exports = { subscribeUser };
