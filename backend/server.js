679562767ac9bf356b299333dfabf4ee7ca6c414
Mihailo Krstic <mihailo.krstic@eng.it>
2025-12-21 18:22:42 +0100

    fix- CORS must be defined  before routes created

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? ["https://swedishmyway.com", "https://www.swedishmyway.com"]
+      : "http://localhost:5000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 const app = express();
 
 // Middleware to parse JSON bodies
@@ -15,6 +25,8 @@ app.use(express.json());
 
 //to accept form data
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors(corsOptions));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "UP!" });
@@ -29,16 +41,5 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Swedish my way app" });
 });
 
-const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? ["https://swedishmyway.com", "https://www.swedishmyway.com"]
-      : "http://localhost:5000",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
